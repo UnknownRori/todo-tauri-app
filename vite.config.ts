eaf8ec0506b2855d8304a24c24488db8078155d6
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-29 16:16:38 +0700

    📦 Install tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import tailwindcss from 'tailwindcss';
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
