f5ce3c76a42727e067fb16321484ced185523095
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-07-30 07:15:40 +0700

    📦 Added load and save menu item

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,7 +12,12 @@ fn greet(name: &str) -> String {
 fn generate_menu() -> Menu {
     // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let submenu = Submenu::new("File", Menu::new().add_item(quit));
+    let save = CustomMenuItem::new("save".to_string(), "Save");
+    let load = CustomMenuItem::new("load".to_string(), "Load");
+    let submenu = Submenu::new(
+        "File",
+        Menu::new().add_item(save).add_item(load).add_item(quit),
+    );
     Menu::new().add_submenu(submenu)
 }
 
